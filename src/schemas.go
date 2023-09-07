8245b5671559baae3f86c4d86b8ed89bdfd93bff
Ruben Villalpando <mr.villalpando@yahoo.com.mx>
2023-09-06 20:56:31 -0600

    added receipt validation

--- src/schemas.go
+++ src/schemas.go
@@ -1,18 +1,14 @@
 package main
 
 type item struct {
-	ShortDescription string `json:"shortDescription"`
-	Price            string `json:"price"`
+	ShortDescription string `json:"shortDescription" validate:"required"`
+	Price            string `json:"price" validate:"required"`
 }
 
 type receipt struct {
-	Retailer     string `json:"retailer"`
-	PurchaseDate string `json:"purchaseDate"`
-	PurchaseTime string `json:"purchaseTime"`
-	Items        []item `json:"items"`
-	Total        string `json:"total"`
-}
-
-type postReceiptResponse struct {
-	id string
+	Retailer     string `json:"retailer" validate:"required"`
+	PurchaseDate string `json:"purchaseDate" validate:"required,datetime=2006-01-02"`
+	PurchaseTime string `json:"purchaseTime" validate:"required,datetime=15:04"`
+	Items        []item `json:"items" validate:"required"`
+	Total        string `json:"total" validate:"required"`
 }
--- src/service.receipts.go
+++ src/service.receipts.go
@@ -2,20 +2,33 @@ package main
 
 import (
 	"fmt"
-	"github.com/gin-gonic/gin"
 	"math/rand"
 	"net/http"
+	"regexp"
 	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 )
 
 var receipts = make(map[string]receipt)
+var validate *validator.Validate
+var retailerRegex = regexp.MustCompile("^\\S+$")
+var totalRegex = regexp.MustCompile("^\\d+\\.\\d{2}$")
+var shortDescRegex = regexp.MustCompile("^[\\w\\s\\-]+$")
+var priceRegex = regexp.MustCompile("^\\d+\\.\\d{2}$")
 
 func postReceipt(c *gin.Context) {
 	var newReceipt receipt
-
 	if err := c.BindJSON(&newReceipt); err != nil {
+		c.AbortWithStatus(http.StatusBadRequest)
 		return
 	}
+	if err := validateReceipt(newReceipt); err != "" {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err})
+		return
+	}
+
 	id := generateId(newReceipt)
 	receipts[id] = newReceipt
 	fmt.Println(receipts)
@@ -38,12 +51,44 @@ func generateId(rec receipt) string {
 	return builder.String()
 }
 
-const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+const digits = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
 
 func generateRandChars(n int) string {
 	builder := strings.Builder{}
 	for i := 0; i < n; i++ {
-		builder.WriteByte(letters[rand.Intn(len(letters))])
+		builder.WriteByte(digits[rand.Intn(len(digits))])
 	}
+	return builder.String()
+}
+
+func validateReceipt(rec receipt) string {
+	validate = validator.New()
+	err := validate.Struct(&rec)
+	builder := strings.Builder{}
+	if err != nil {
+		for _, err := range err.(validator.ValidationErrors) {
+			builder.WriteString(err.Error())
+			builder.WriteString("\n")
+		}
+	}
+
+	validRetailer := retailerRegex.MatchString(rec.Retailer)
+	validTotal := totalRegex.MatchString(rec.Total)
+
+	if !validRetailer {
+		builder.WriteString("Invalid Retailer\n")
+	}
+	if !validTotal {
+		builder.WriteString("Invalid Total\n")
+	}
+	for i, item := range rec.Items {
+		if !priceRegex.MatchString(item.Price) {
+			builder.WriteString(fmt.Sprintf("Invalid Price on item #%d\n", i))
+		}
+		if !shortDescRegex.MatchString(item.ShortDescription) {
+			builder.WriteString(fmt.Sprintf("Invalid short description on item #%d\n", i))
+		}
+	}
+
 	return builder.String()
 }
